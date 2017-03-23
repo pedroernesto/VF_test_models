9f058aaccc0a48ebcc4afd2b0cda5149783a2aea
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2017-03-23 15:09:47 +0100

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,25 +1,27 @@
 import sciunit
 import quantities as pq
+from neuronunit.capabilities import ProvidesLayerInfo
 from hbp_validation_framework.versioning import Versioned
 
 class testCell(sciunit.Model, ProvidesLayerInfo, Versioned):
     id = "9ade6831-a758-42be-a50e-d5cb65859c34"
 
-    def __init__(self, name="testCell", layer_info=[]):
+    def __init__(self, name="testCell", layer_info={}):
         self.layer_info = layer_info
         sciunit.Model.__init__(self, name=name)
         self.name = name
         self.description = "Dummy cell for testing layer heights"
+        self.set_layer_info_default()
 
     def set_layer_info(self, layer_info):
         self.layer_info = layer_info
 
     def set_layer_info_default(self):
-        self.layer_info = [ ["layer I", 150.0*pq.um],
-                            ["layer II-III", 300.0*pq.um],
-                            ["layer IV", 250.0*pq.um],
-                            ["layer V", 425.0*pq.um],
-                            ["layer VI", 675.0*pq.um] ]
+        self.layer_info = { "layer_I": {'height': {'value':'150 um'}},
+                            "layer_II-III": {'height': {'value':'300 um'}},
+                            "layer_IV": {'height': {'value':'250 um'}},
+                            "layer_V": {'height': {'value':'425 um'}},
+                            "layer_VI": {'height': {'value':'675 um'}} }
 
     def get_layer_info(self):
         return self.layer_info
---
+++ run_tests.py
@@ -0,0 +1,43 @@
+"""
+Main script for running vaidation tests of hippocampus CA1 pyramidal neuron models
+
+Author: Andrew P. Davison, CNRS
+Date: February 2017
+"""
+
+import argparse
+from datetime import datetime
+from hbp_validation_framework import ValidationTestLibrary
+from hbp_validation_framework.datastores import CollabDataStore
+import models
+
+parser = argparse.ArgumentParser()
+parser.add_argument("model",
+                    help="name of the model to test, e.g. Bianchi, Golding, KaliFreund or Migliore"),
+parser.add_argument("test",
+                    help="test configuration file (local path or URL)")
+config = parser.parse_args()
+
+# Load the model
+model = getattr(models, config.model)()
+
+# Load the test
+# checks the test is registered with the Validation framework,
+# if it is not, fail with instructions for registering,
+# or offer to register it
+# test_library = ValidationTestLibrary() # default url for HBP service
+test_library = ValidationTestLibrary(username="shailesh") # default url for HBP service
+test = test_library.get_validation_test(config.test,
+                                        force_run=False,
+                                        show_plot=True)
+
+# Run the test
+score = test.judge(model, deep_error=True)
+
+# Register the result with the HBP Validation service
+# This could be integrated into test.judge() if we extend sciunit appropriately
+collab_folder = "{}_{}".format(config.model, datetime.now().strftime("%Y%m%d-%H%M%S"))
+collab_storage = CollabDataStore(username="shailesh",
+                                 collab_id="1771",
+                                 base_folder=collab_folder)
+test_library.register(score, collab_storage)  # score already linked to test and model (I think)
