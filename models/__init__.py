d777806d767c124bea54ba35e0df8631a6bae16c
Pedro Garcia <pedro.garcia@unic.cnrs-gif.fr>
2017-08-16 14:49:15 +0200

    Updating Models classes

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,9 @@
 import os
 import sciunit
-#from neuronunit.capabilities import ProvidesLayerInfo
 from hbp_validation_framework.versioning import Versioned
-#import morphounit.capabilities as cap
 
 #==============================================================================
 
-#class testColumn(sciunit.Model, ProvidesLayerInfo, Versioned):
 class testColumn(sciunit.Model, Versioned):
     id = "9ade6831-a758-42be-a50e-d5cb65859c34"
 
@@ -22,18 +19,17 @@ class testColumn(sciunit.Model, Versioned):
 
     def set_layer_info_default(self):
         self.layer_info = { "Layer 1": {'height': {'value':'165 um'}},
-                    "Layer 2": {'height': {'value':'149 um'}},
-                    "Layer 3": {'height': {'value':'353 um'}},
-                    "Layer 4": {'height': {'value':'190 um'}},
-                    "Layer 5": {'height': {'value':'525 um'}},
-                    "Layer 6": {'height': {'value':'700 um'}} }
+                            "Layer 2": {'height': {'value':'149 um'}},
+                            "Layer 3": {'height': {'value':'353 um'}},
+                            "Layer 4": {'height': {'value':'190 um'}},
+                            "Layer 5": {'height': {'value':'525 um'}},
+                            "Layer 6": {'height': {'value':'700 um'}} }
 
     def get_layer_info(self):
         return self.layer_info
 
 #==============================================================================
 
-#class hippoCircuit(sciunit.Model, cap.ProvidesDensityInfo, Versioned):
 class hippoCircuit(sciunit.Model, Versioned):
         id = "f25d05b2-2358-418b-8914-fe02a412ac74"
 
@@ -50,6 +46,25 @@ class hippoCircuit(sciunit.Model, Versioned):
         def get_density_info(self):
             return self.density_info
 
+#==============================================================================
+
+class NeuroM_NeuriteLength(sciunit.Model, Versioned):
+
+    id = "afc85429-0db2-4414-8fc7-3ed5781a5019"
+
+    def __init__(self, name='NeuriteLength', NeuriteLength_info={}):
+        self.NeuriteLength_info = NeuriteLength_info
+        sciunit.Model.__init__(self, name=name)
+        self.name = name
+        self.description = "Dummy model for testing neurite length"
+        self.set_NeuriteLength_info_default()
+
+    def set_NeuriteLength_info_default(self):
+        self.NeuriteLength_info = {'NeuriteLength': {'value': '60 um'}}
+
+    def get_NeuriteLength_info(self):
+        return self.NeuriteLength_info
+    
 #==============================================================================
 
 #class neuroM_loader(sciunit.Model, cap.HandlesNeuroM, Versioned):
@@ -81,5 +96,5 @@ class neuroM_loader(sciunit.Model, Versioned):
 
         def get_soma_diameter_info(self):
             return self.soma_diameter
-
+        
 #==============================================================================
