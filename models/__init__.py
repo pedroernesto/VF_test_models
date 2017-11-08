6315656be80e6c59f3ae45f10837fe2e7a15919a
Pedro Garcia <pedro.garcia@unic.cnrs-gif.fr>
2017-11-08 11:44:52 +0100

    Adding a dummy model for synapses laminar distribution across CA1 layers

--- models/__init__.py
+++ models/__init__.py
@@ -84,15 +84,35 @@ class NeuroM_NeuriteLength(sciunit.Model, Versioned):
 
     id = "afc85429-0db2-4414-8fc7-3ed5781a5019"
 
-    def __init__(self, name='NeuriteLength', NeuriteLength_info={}):
-        self.NeuriteLength_info = NeuriteLength_info
+    def __init__(self, name='NeuriteLength', model_path=None, cell_part=None,
+                 cell_feature_name='terminal_path_lengths_per_neurite', NeuriteLength_info={}):
+
         sciunit.Model.__init__(self, name=name)
         self.name = name
-        self.description = "Dummy model for testing neurite length"
-        self.set_NeuriteLength_info_default()
+        self.description = "Model for testing neurite (euclidean) length"
+        self.NeuriteLength_info = NeuriteLength_info
 
-    def set_NeuriteLength_info_default(self):
-        self.NeuriteLength_info = {'NeuriteLength': {'value': '60 um'}}
+        if model_path == None:
+            print "Please specify the path to the morphology file or the directory"
+            quit()
+        if os.path.isdir(model_path):
+            print "Directory"
+            quit()
+
+        if cell_part not in ['SOMA', 'AXON', 'APICAL_DENDRITE', 'BASAL_DENDRITE']:
+            print "Please, specify a valid cell part: 'SOMA', 'AXON', 'APICAL_DENDRITE', 'BASAL_DENDRITE'"
+            quit()
+
+        self.morph_path = model_path
+        self.cell_part = cell_part
+        self.cell_feature_name = cell_feature_name
+        self.set_NeuriteLength_info()
+
+    def set_NeuriteLength_info(self):
+        import neurom as nm
+        nrn = nm.load_neuron(self.morph_path)
+        feature_value = nm.get(self.cell_feature_name, nrn, neurite_type=eval('nm.'+self.cell_part))
+        self.NeuriteLength_info = feature_value
 
     def get_NeuriteLength_info(self):
         return self.NeuriteLength_info
@@ -119,4 +139,46 @@ class CA1Layers_NeuritePathDistance(sciunit.Model, Versioned):
 
     def get_CA1LayersNeuritePathDistance_info(self):
         return self.CA1LayersNeuritePathDistance_info
+
+#==============================================================================
+
+class CA1_laminar_distribution_synapses(sciunit.Model, Versioned):
+
+    id = ""
+    version = "Project42-EPFL-v0.1"
+
+    import bluepy
+    from bluepy.v2.enums import Synapse
+    from voxcell import VoxelData, RegionMap
+
+    import json
+
+    def __init__(self, name="CA1_laminar_distribution_synapses", CA1_laminar_distribution_synapses_info={}):
+
+	'''
+	# Model releases on Julia/Juron clusters
+	circuit_path = '/work/pra098/pra09801/CircuitRelease/r2017.06b/CircuitConfig'
+	volume_path = '/work/pra098/pra09801/CircuitRelease/r2017.06b/volume/volume.nrrd'
+	hierarchy_path = '/work/pra098/pra09801/CircuitRelease/r2017.06b/volume/hierarchy.json'
+
+        self.circuit = bluepy.Circuit(circuit_path)
+        self.annotation = VoxelData.load_nrrd(volume_path)
+        self.region_map = RegionMap.from_json(hierarchy_path)
+	'''
+
+        sciunit.Model.__init__(self, name=name)
+        self.name = name
+        self.description = "HBP Hippocampus's output to test synapses distribution across CA1 layers"
+	self.CA1_laminar_distribution_synapses_info = CA1_laminar_distribution_synapses_info
+        self.set_CA1_laminar_distribution_synapses_info_default()
+
+
+    def set_CA1_laminar_distribution_synapses_info_default(self):
+        model_prediction_path = "./model_predictions/CA1_laminar_distribution_synapses_HBPmod.json" 
+	json_data=open(model_prediction_path).read()
+        self.CA1_laminar_distribution_synapses_info = json.loads(json_data)
+
+
+    def get_CA1_laminar_distribution_synapses_info(self):
+        return self.CA1_laminar_distribution_synapses_info
 
