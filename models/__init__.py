37cf62a7491f0c937eb2f3d28ce35a713d91e477
Pedro Garcia <pedro.garcia@unic.cnrs-gif.fr>
2017-12-14 18:36:59 +0100

    Adding figures related with a test for synapses distribution across CA1 layers

--- models/__init__.py
+++ models/__init__.py
@@ -6,32 +6,6 @@ import json
 
 #==============================================================================
 
-class testColumn(sciunit.Model, Versioned):
-    instance_id = "9ade6831-a758-42be-a50e-d5cb65859c34"
-
-    def __init__(self, name="testColumn", layer_info={}):
-        self.layer_info = layer_info
-        sciunit.Model.__init__(self, name=name)
-        self.name = name
-        self.description = "Dummy model for testing layer heights"
-        self.set_layer_info_default()
-
-    def set_layer_info(self, layer_info):
-        self.layer_info = layer_info
-
-    def set_layer_info_default(self):
-        self.layer_info = { "Layer 1": {'height': {'value':'165 um'}},
-                            "Layer 2": {'height': {'value':'149 um'}},
-                            "Layer 3": {'height': {'value':'353 um'}},
-                            "Layer 4": {'height': {'value':'190 um'}},
-                            "Layer 5": {'height': {'value':'525 um'}},
-                            "Layer 6": {'height': {'value':'700 um'}} }
-
-    def get_layer_info(self):
-        return self.layer_info
-
-#==============================================================================
-
 class hippoCircuit(sciunit.Model, Versioned):
         instance_id = "f25d05b2-2358-418b-8914-fe02a412ac74"
 
